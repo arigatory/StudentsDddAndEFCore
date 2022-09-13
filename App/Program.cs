1d6400448490475e1fa0a3230b38ba501764a559
Ivan MSI <arigatory@gmail.com>
2022-09-13 09:58:28 +0300

    done with setup

--- App/Program.cs
+++ App/Program.cs
@@ -1,10 +1,50 @@
-﻿namespace App
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace App;
+
+public class Program
 {
-    internal class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string connectionString = GetConnectionString();
+        ILoggerFactory loggerFactory = CreateLoggerFactory();
+
+        var optionBuilder = new DbContextOptionsBuilder<SchoolContext>();
+        optionBuilder
+            .UseSqlServer(connectionString)
+            .UseLoggerFactory(loggerFactory)
+            .EnableSensitiveDataLogging();
+        using (var context = new SchoolContext(optionBuilder.Options))
         {
-            Console.WriteLine("Hello, World!");
+            Student? student = context.Students.Find(1L);
+            if (student is not null)
+            {
+                student.Name += 2;
+            }
+
+            context.SaveChanges();
         }
+    }
+
+    private static ILoggerFactory CreateLoggerFactory()
+    {
+        return LoggerFactory.Create(builder =>
+        {
+            builder.AddFilter((category, level) =>
+                category == DbLoggerCategory.Database.Command.Name && level == LogLevel.Information)
+                    .AddConsole();
+        });
+    }
+
+    private static string GetConnectionString()
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        return configuration["ConnectionString"];
     }
 }
